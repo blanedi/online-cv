69df0f062968a2f3b7352f7b3b4bfe7a7edb18d2
blanedi <42097205+blanedi@users.noreply.github.com>
2025-09-12 14:42:58 +0200

    Save out in commit for website generation

---
+++ out/_next/static/chunks/6941a05e458ef55b.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/a41dbe67f1447a5e.js",
+  "static/chunks/72a9d511183c5f7f.js",
+  "static/chunks/turbopack-7919651171b9d6fb.js"
+])
---
+++ out/_next/static/e8WAxfa09JiQyj-52_Dz1/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/097b51b70d90f519.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/online-cv//_next/:path+",
+        "destination": "/online-cv/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
